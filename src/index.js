e51d0919408006e1c32b322271df313e5a407098
Indra Firmansyah <indransyah@gmail.com>
2018-10-28 13:05:32 +0700

    add missing dynamic component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Loadable from 'react-loadable';
 import { Provider } from 'react-redux';
+import Loading from './components/Loading';
 import configureStore from './configureStore';
+
+const App = Loadable({
+  loader: () => import('./App'),
+  loading: Loading,
+});
 
 const store = configureStore();
 
