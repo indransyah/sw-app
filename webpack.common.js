ccfdd623140dbf44e3883861c3ec3dd212fa9cc5
Indra Firmansyah <indransyah@gmail.com>
2018-10-29 06:52:03 +0700

    temp2

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,14 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Star Wars React Redux App',
+      template: './src/assets/index.html',
+    }),
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
